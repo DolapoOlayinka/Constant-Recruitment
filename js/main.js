88bb2bc9639e05fd294984c0d55ad6e9499efb4a
DolapoOlayinka <dolapoolayinka0@gmail.com>
2020-07-08 18:01:36 +0100

    New index page feature

--- js/main.js
+++ js/main.js
@@ -8,7 +8,13 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
-	
+	/* 1. Proloder */
+    $(window).on('load', function () {
+		$('#preloader-active').delay(450).fadeOut('slow');
+		$('body').delay(450).css({
+		  'overflow': 'visible'
+		});
+	  });
 
 	var siteMenuClone = function() {
 
